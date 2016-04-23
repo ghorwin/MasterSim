e9244e711d47e0b38772d800e75d84290af51531
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2016-04-23 15:25:25 +0000

    Added third-party library

--- MasterSim/src/MSIM_Project.h
+++ MasterSim/src/MSIM_Project.h
@@ -70,9 +70,6 @@ public:
 	/*! Operation mode of the master algorithm. */
 	MasterMode		m_masterMode;
 
-	// MasterDebug - replaced by command-line argument --verbosity-level
-	// OutputGnuplot - replaced by output mode parameter
-
 	/*! Maximum number of iterations per communication step (within each priority/cycle). */
 	unsigned int	m_maxSteps;
 
@@ -81,34 +78,15 @@ public:
 	/*! Relative tolerance - used for convergence check and for time integration error control. */
 	double			m_relTol;
 
+	/*! Minimum output time step. */
+	double			m_tOutputStepMin;
+
 	/*! All simulators coupled in this master scenario. */
 	std::vector<SimulatorDef>	m_simulators;
 
 	/*! Defines the connection graph. */
 	std::vector<GraphEdge>		m_graph;
 
-//	simulator   0   fmus/simx/Part1.fmu
-//	simulator   1   fmus/simx/Part2.fmu
-//	simulator   2   fmus/simx/Part3.fmu
-//	graph #val #sim -1(out)/1(in) valueref
-//	0   0   -1      r Part1.x2
-//	0   1    1      r Part2.x2
-//	1   0   -1      r Part1.x1
-//	1   1    1      r Part2.x1
-//	2   1   -1      r Part2.x3
-//	2   2    1      r Part3.x3
-//	3   2   -1      r Part3.x4
-//	3   1    1      r Part2.x4
-//	end
-//	priority #sim priority
-//	0   0
-//	1   1
-//	2   1
-//	end
-//	cycles  #prior    0(no)/1(yes)
-//	0   0
-//	1   1
-//	end
 };
 
 } // namespace MASTER_SIM
---
+++ third-party/readme.txt
@@ -0,0 +1,2 @@
+This directory holds the release versions of third-party libraries.
+
