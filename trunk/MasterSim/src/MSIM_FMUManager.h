00138451d1be4b203b34848d264493987a8b800f
ghorwin <ghorwin@dd098b5a-9c97-4e98-abe7-e951c7a10ce5>
2016-04-27 05:14:36 +0000

    Interface for importing FMUs done.

--- trunk/MasterSim/src/MSIM_FMUManager.h
+++ trunk/MasterSim/src/MSIM_FMUManager.h
@@ -22,18 +22,21 @@ public:
 
 	/*! Imports an FMU.
 		The actual import path (unzip directory) is auto-generated.
-		\param fmuBaseDirectory Base directory for FMU slaves
+		\param fmuTargetDirectory Target directory for extracted FMUs.
 		\param fmuFilePath Full path to FMU-file (from master project file).
 		\return Returns a pointer to the FMUSlave instance that contains all data provided by this FMU.
 
 		\note Throws an exception if an error occurs. In this case, all resources are deallocated again.
 	*/
-	FMU * importFMU(const IBK::Path & fmuBaseDirectory, const IBK::Path & fmuFilePath);
+	void importFMU(const IBK::Path & fmuTargetDirectory, const IBK::Path & fmuFilePath);
 
 	/*! Alternative version of FMU import where unzip directory is provided by user and not auto-generated.
 		If another FMU had been instantiated with same unzip directory, an IBK::Exception will be thrown.
 	*/
-	FMU * importFMU(const IBK::Path & fmuBaseDirectory, const IBK::Path & fmuFilePath, const IBK::Path & unzipPath);
+	void importFMUAt(const IBK::Path & fmuFilePath, const IBK::Path & unzipPath);
+
+	/*! Convenience function, returns FMU by file path to fmu archive as passed to importFMU. */
+	FMU * fmuByPath(const IBK::Path & fmuFilePath);
 
 	/*! If true, FMUs are unzipped to directories first, before ModelDescriptions are read. */
 	bool	m_unzipFMUs;
@@ -41,15 +44,13 @@ public:
 	/*! If true, debug logging during FMU unzipping/ModelDescription reading is enabled. */
 	bool	m_debugLogging;
 
+	/*! All FMUs already imported. */
+	std::vector<FMU*>	m_fmus;
+
 private:
 	/*! Generates a unique FMU file path based on fmu base directory and fmuFilePath. */
 	IBK::Path generateFilePath(const IBK::Path & fmuBaseDirectory, const IBK::Path & fmuFilePath);
 
-	/*! Utility function to unzip FMU. */
-	void unzipFMU(const IBK::Path & pathToFMU, const IBK::Path & extractionPath);
-
-	/*! All FMUs already imported. */
-	std::vector<FMU*>	m_fmus;
 };
 
 } // namespace MASTER_SIM
