bf93c09dccdcc5c9e2fb068c7732838ade318033
ghorwin <ghorwin@dd098b5a-9c97-4e98-abe7-e951c7a10ce5>
2016-04-27 19:20:01 +0000

    Build batch file for MinGW32 builds added. Updated CMakeLists.txt

---
+++ trunk/build/cmake/build_mingw32.bat
@@ -0,0 +1,27 @@
+@echo off
+
+:: These environment variables can also be set externally
+if not defined JOM_PATH (
+	set JOM_PATH=c:\Qt\qtcreator-3.2.2\bin
+)
+if not defined QMAKE_PATH (
+	set QMAKE_PATH=c:\Qt\4.8.6\bin\qmake.exe
+)
+
+:: add search path for jom.exe
+set PATH=%PATH%;c:\mingw32\bin;%JOM_PATH%
+
+:: create and change into build subdir
+mkdir bb_mingw32
+pushd bb_mingw32
+
+:: configure makefiles and build
+cmake -G "MinGW Makefiles" .. -DCMAKE_BUILD_TYPE:String="Release" -DQT_QMAKE_EXECUTABLE=%QMAKE_PATH%
+jom
+
+popd
+
+:: copy executable to bin/release dir
+xcopy /Y .\bb_mingw32\MasterSimulator\MasterSimulator.exe ..\..\bin\release
+xcopy /Y .\bb_mingw32\MasterSimulatorUI\MasterSimulatorUI.exe ..\..\bin\release
+
