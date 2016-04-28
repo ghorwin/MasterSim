28c949a55bff54cbf92108ab591b1149265308a0
ghorwin <ghorwin@dd098b5a-9c97-4e98-abe7-e951c7a10ce5>
2016-04-28 19:02:45 +0000

    Added test script.

---
+++ trunk/data/tests/import_tests.bat
@@ -0,0 +1,4 @@
+..\..\bin\release_mingw32\MasterSimulator.exe init_test_win32_vc10.cfg -t --verbosity-level=3
+pause
+..\..\bin\release\MasterSimulator.exe init_test_win32_vc10.cfg -t --verbosity-level=3
+pause
