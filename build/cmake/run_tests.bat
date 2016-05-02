bcedafb0f75a5b0086aa903ec53f309a2b8ee5b7
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2016-05-02 14:08:27 +0000

    Updated test scripts.

--- build/cmake/run_tests.bat
+++ build/cmake/run_tests.bat
@@ -2,6 +2,11 @@
 
 set PATH=c:\Qt\4.8.6_VC10\bin;%PATH%
 
-python ..\..\scripts\tests\run_tests.py -p ..\..\data\tests -s ..\..\bin\release\MasterSimulator.exe -e sim
+pushd ..\..\data\tests
+..\..\bin\release\MasterSimulator.exe init_test_win32_vc10.cfg -t --verbosity-level=2
+popd
+
+
+:: python ..\..\scripts\tests\run_tests.py -p ..\..\data\tests -s ..\..\bin\release\MasterSimulator.exe -e sim
 
 pause
--- build/cmake/run_tests.sh
+++ build/cmake/run_tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-python ../../scripts/tests/run_tests.py -p ../../data/tests -s ../../bin/release/MasterSimulator -e sim $*
+cd ../../data/tests
+../../bin/release/MasterSimulator init_test_linux64.cfg -t --verbosity-level=3
+cd -
+
+#python ../../scripts/tests/run_tests.py -p ../../data/tests -s ../../bin/release/MasterSimulator -e sim $*
 
 
