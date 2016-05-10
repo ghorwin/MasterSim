ee024e1313461d0a412a30eb544fa015e8ec2870
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2016-05-10 15:36:14 +0000

    Added test script for running tests.

---
+++ data/tests/run_tests.bat
@@ -0,0 +1,5 @@
+..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussJacobi_win32.cfg
+..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussSeidelNonIterative_win32.cfg
+..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussSeidelIterative_win32.cfg
+pause
+
