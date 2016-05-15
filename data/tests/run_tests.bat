d0f3cf054c4bb885530cc3c3817223b591659ef7
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2016-05-15 18:32:45 +0000

    Tests updated. Added test for error control.

--- data/tests/run_tests.bat
+++ data/tests/run_tests.bat
@@ -1,5 +1,6 @@
 ..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussJacobi_win32.cfg -x
 ..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussSeidelNonIterative_win32.cfg -x
 ..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussSeidelIterative_win32.cfg -x
+..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussSeidelIterativeErrorTest_win32.cfg -x
 pause
 
