66f6699342d4b9329490d8a1c52892d4f5a6a4e5
ghorwin <ghorwin@dd098b5a-9c97-4e98-abe7-e951c7a10ce5>
2016-05-22 06:41:22 +0000

    Added translation files.

---
+++ trunk/MasterSimulatorUI/resources/translations/MasterSimulatorUI_de.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/MainWindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/MainWindow.ui" line="34"/>
+        <source>There will be code...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/MainWindow.ui" line="67"/>
+        <source>&amp;File</source>
+        <translation>&amp;Datei</translation>
+    </message>
+    <message>
+        <location filename="../../src/MainWindow.ui" line="84"/>
+        <source>Quit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="../../src/MainWindow.ui" line="87"/>
+        <source>Ctrl+Q</source>
+        <translation>Strg + Q</translation>
+    </message>
+</context>
+</TS>
