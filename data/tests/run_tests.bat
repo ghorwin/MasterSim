7dd0bb5e05be85d7cf53ec16843cee980edd1a82
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2016-06-03 06:31:14 +0000

    Updated test cases to new syntax.

--- data/tests/run_tests.bat
+++ data/tests/run_tests.bat
@@ -1,6 +1,10 @@
-..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussJacobi_win32.cfg -x
-..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussSeidelNonIterative_win32.cfg -x
-..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussSeidelIterative_win32.cfg -x
-..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussSeidelIterativeErrorTest_win32.cfg -x
+..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussJacobi_win32.cfg -x -t
+..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussSeidelNonIterative_win32.cfg -x -t
+..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussSeidelIterative_win32.cfg -x -t
+..\..\bin\release\MasterSimulator.exe Math_003_control_loop\Math003_SimX_GaussSeidelIterativeErrorTest_win32.cfg -x -t
+
+..\..\bin\release\MasterSimulator.exe Math_019_lin_equ_small\Math019_SimX_GaussSeidelIterativeErrorTest_win32.cfg -x -t
+..\..\bin\release\MasterSimulator.exe Math_019_lin_equ_small\Math019_SimX_NewtonErrorTest_win32.cfg -x -t
+
 pause
 
