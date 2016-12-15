5ab788a98bcb80f4f16aa28776ad28c03d6843af
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2016-12-15 20:38:37 +0000

    Updated test scripts (corresponding python scripts still need to be fixed).

--- build/cmake/run_tests.sh
+++ build/cmake/run_tests.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-cd ../../data/tests
-../../bin/release/MasterSimulator init_test_linux64.cfg -t --verbosity-level=3
-cd -
-
-#python ../../scripts/tests/run_tests.py -p ../../data/tests -s ../../bin/release/MasterSimulator -e sim $*
-
-
+python ../../scripts/tests/run_tests.py -p ../../data/tests -s ../../bin/release/MasterSimulator -e msim $*
