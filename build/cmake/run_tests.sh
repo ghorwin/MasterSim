5fe1e452c868455411fe9d345a93b59949fadfcf
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2017-11-17 21:53:20 +0000

    Even more reorganization of tests.

--- build/cmake/run_tests.sh
+++ build/cmake/run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../../scripts/tests/run_tests.py -p ../../data/tests -s ../../bin/release/MasterSimulator -e msim $*
+python ../../scripts/tests/run_tests.py -p ../../data/tests/linux64 -s ../../bin/release/MasterSimulator -e msim $*
