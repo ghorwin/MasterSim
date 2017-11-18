0453af6873b1275a54d161955aa642efad374104
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2017-11-18 16:34:39 +0000

    Renamed test cases and updated ignore properties.

--- data/tests/win32/Math_003_control_loop/run.bat
+++ data/tests/win32/Math_003_control_loop/run.bat
@@ -1,10 +1,10 @@
 @echo off
 set MASTERSIM=..\..\..\..\bin\release\MasterSimulator.exe
 
-%MASTERSIM% Math003_SimX_GaussJacobi_1iters_fixedStep_win32.msim -x
-%MASTERSIM% Math003_SimX_GaussJacobi_1iters_variableStep_Richardson_win32.msim -x
-%MASTERSIM% Math003_SimX_GaussSeidel_1iters_fixedStep_win32.msim -x
-%MASTERSIM% Math003_SimX_GaussSeidel_1iters_variableStep_Richardson_win32.msim -x
-%MASTERSIM% Math003_SimX_GaussSeidel_6iters_variableStep_Richardson_win32.msim -x
-%MASTERSIM% Math003_SimX_GaussSeidel_6iters_variableStep_win32.msim -x
+%MASTERSIM% Math003_SimX_GaussJacobi_1iters_fixedStep.msim -x
+%MASTERSIM% Math003_SimX_GaussJacobi_1iters_variableStep_Richardson.msim -x
+%MASTERSIM% Math003_SimX_GaussSeidel_1iters_fixedStep.msim -x
+%MASTERSIM% Math003_SimX_GaussSeidel_1iters_variableStep_Richardson.msim -x
+%MASTERSIM% Math003_SimX_GaussSeidel_6iters_variableStep_Richardson.msim -x
+%MASTERSIM% Math003_SimX_GaussSeidel_6iters_variableStep.msim -x
 pause
