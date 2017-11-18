a0d28799f629e3bbf37899a3b1443f5590d10a73
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2017-11-18 16:44:05 +0000

    Minor update in CMakeLists (VC warnings).

--- build/cmake/run_tests.bat
+++ build/cmake/run_tests.bat
@@ -2,6 +2,8 @@
 
 ::set PATH=c:\Qt\4.8.6_VC10\bin;%PATH%
 
-python ..\..\scripts\tests\run_tests.py -p ..\..\data\tests -s ..\..\bin\release\MasterSimulator.exe -e msim
+
+echo *** Running win32 tests ***
+python ..\..\scripts\tests\run_tests.py -p ..\..\data\tests\win32 -s ..\..\bin\release\MasterSimulator.exe -e msim
 
 pause
