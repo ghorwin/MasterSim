caec38129b867708df996197ce284ca6c35da329
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2017-11-20 08:48:13 +0000

    More work on analyzing example.

--- data/tests/win32/Math_003_control_loop/run.bat
+++ data/tests/win32/Math_003_control_loop/run.bat
@@ -5,6 +5,6 @@ set MASTERSIM=..\..\..\..\bin\release\MasterSimulator.exe
 %MASTERSIM% Math003_SimX_GaussJacobi_1iters_variableStep_Richardson.msim -x
 %MASTERSIM% Math003_SimX_GaussSeidel_1iters_fixedStep.msim -x
 %MASTERSIM% Math003_SimX_GaussSeidel_1iters_variableStep_Richardson.msim -x
-%MASTERSIM% Math003_SimX_GaussSeidel_6iters_variableStep_Richardson.msim -x
-%MASTERSIM% Math003_SimX_GaussSeidel_6iters_variableStep.msim -x
+%MASTERSIM% Math003_SimX_GaussSeidel_2iters_variableStep_Richardson.msim -x
+%MASTERSIM% Math003_SimX_GaussSeidel_3iters_variableStep_Richardson.msim -x
 pause
