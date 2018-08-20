4f15881d10cad51387322e06a293da1d9b88f7db
ghorwin <ghorwin@dd098b5a-9c97-4e98-abe7-e951c7a10ce5>
2018-08-20 19:40:10 +0000

    Build script fix for macos

--- trunk/build/cmake/run_tests.sh
+++ trunk/build/cmake/run_tests.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python ../../scripts/TestSuite/run_tests.py -p ../../data/tests/linux64 -s ../../bin/release/MasterSimulator -e msim $*
+if [ -e ../../bin/release/MasterSimulatorUI.app ]; then
+  python ../../scripts/TestSuite/run_tests.py -p ../../data/tests/darwin64 -s ../../bin/release/MasterSimulator -e msim $*
+else
+  python ../../scripts/TestSuite/run_tests.py -p ../../data/tests/linux64 -s ../../bin/release/MasterSimulator -e msim $*
+fi
