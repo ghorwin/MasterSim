0fc5438325096ba3093ce51930410214d5472eb2
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2019-07-15 16:10:36 +0000

    Fixed undefined behavior on startup page.

--- MasterSimulatorUI/src/MSIMSettings.cpp
+++ MasterSimulatorUI/src/MSIMSettings.cpp
@@ -60,6 +60,8 @@ void MSIMSettings::setDefaults() {
 
 	m_maxNumUNDOSteps = 10000;
 
+	m_thumbNailSize = 0; // for now we do not have thumbnails, yet
+
 	// determine text executable
 	m_textEditorExecutable.clear();
 #ifdef Q_OS_UNIX
