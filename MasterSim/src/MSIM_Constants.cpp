6b3ed0ffe51a202525ef2e11269fc0e4f1445dd6
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2019-08-03 05:55:34 +0000

    Implemented tickets #54 and #56, increased version number to 0.7.1

--- MasterSim/src/MSIM_Constants.cpp
+++ MasterSim/src/MSIM_Constants.cpp
@@ -4,7 +4,7 @@ namespace MASTER_SIM {
 
 const char * const VERSION = "0.7";
 
-const char * const LONG_VERSION = "0.7.0";
+const char * const LONG_VERSION = "0.7.1";
 
 const char * const PROGRAM_NAME = "MasterSim 0.7";
 
