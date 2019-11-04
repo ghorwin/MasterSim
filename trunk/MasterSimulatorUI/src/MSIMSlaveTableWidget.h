932723e85a3d15e8af8e3c0da83ea64249e0dcac
ghorwin <ghorwin@dd098b5a-9c97-4e98-abe7-e951c7a10ce5>
2019-11-04 16:59:01 +0000

    Implemented proper resize handling of slave table widget.

--- trunk/MasterSimulatorUI/src/MSIMSlaveTableWidget.h
+++ trunk/MasterSimulatorUI/src/MSIMSlaveTableWidget.h
@@ -1,12 +1,18 @@
-#ifndef MSIMSLAVETABLEWIDGET_H
-#define MSIMSLAVETABLEWIDGET_H
+#ifndef MSIMSlaveTableWidgetH
+#define MSIMSlaveTableWidgetH
 
 #include <QTableWidget>
 
+/*! The table widget in the slaves view that shows the simulators/slaves.
+	Subclassed to provide custom resize operation.
+*/
 class MSIMSlaveTableWidget : public QTableWidget {
 	Q_OBJECT
 public:
 	MSIMSlaveTableWidget(QWidget * parent);
+
+protected:
+	void resizeEvent(QResizeEvent *event) override;
 };
 
-#endif // MSIMSLAVETABLEWIDGET_H
+#endif // MSIMSlaveTableWidgetH
