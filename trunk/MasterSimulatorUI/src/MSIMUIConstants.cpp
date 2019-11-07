76d732eb4b1d61002e19aec03ce9075fa3c54ce1
ghorwin <ghorwin@dd098b5a-9c97-4e98-abe7-e951c7a10ce5>
2019-11-07 20:05:20 +0000

    Major improvement of parameter editor.

--- trunk/MasterSimulatorUI/src/MSIMUIConstants.cpp
+++ trunk/MasterSimulatorUI/src/MSIMUIConstants.cpp
@@ -2,8 +2,10 @@
 
 #include <QFont>
 #include <QFontMetrics>
+#include <QTableView>
 #include <QTableWidget>
 #include <QHeaderView>
+#include <QApplication>
 
 const char * const ORG_NAME				= "IBK";
 const char * const DOT_FILE_EXTENSION	= ".msim";
@@ -25,11 +27,37 @@ const char * const FIXED_FONT_FAMILY = "Courier New";
 
 #endif
 
-void formatTable(QTableWidget * tab) {
-	QFont f;
-	f.setPointSize(TABLE_FONT_SIZE);
-	tab->setFont(f);
+//void formatTable(QTableWidget * tab) {
+//	QFont f;
+//	f.setPointSize(TABLE_FONT_SIZE);
+//	tab->setFont(f);
+//	QFontMetrics fm(f);
+//	int pixels = fm.lineSpacing()+8;
+//	tab->verticalHeader()->setDefaultSectionSize(pixels);
+//}
+
+
+void formatTable(QTableView * tab, bool caption) {
+	QFont f = qApp->font();
+	//int ps = f.pointSize();
+	//f.setPointSize(ps-1);
 	QFontMetrics fm(f);
-	int pixels = fm.lineSpacing()+8;
+	int pixels = fm.lineSpacing();
+	int additionalGap = pixels*0.2;
+#ifdef Q_OS_LINUX
+	if (additionalGap < 3)
+		additionalGap = 3;
+#elif defined(Q_OS_MAC)
+	if (additionalGap < 2)
+		additionalGap = 2;
+#else
+	additionalGap = pixels*0.4;
+	if (additionalGap < 4)
+		additionalGap = 4;
+#endif
+	if (caption)
+		additionalGap *= 2;
+	pixels += additionalGap;
 	tab->verticalHeader()->setDefaultSectionSize(pixels);
 }
+
