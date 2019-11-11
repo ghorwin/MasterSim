7571e785a44df6512466f16c7ba2fa0f1d0d8314
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2019-11-11 19:18:18 +0000

    Improved syntax highlighting.

--- doc/manual/build.sh
+++ doc/manual/build.sh
@@ -3,4 +3,4 @@
 echo '*** Generating html ***'
 asciidoctor MasterSim_manual.adoc
 echo '*** Generating pdf ***'
-asciidoctor-pdf -a pdf-theme=mastersim-manual-pdf-theme.yml MasterSim_manual.adoc
+asciidoctor-pdf -a pdf-theme=mastersim-manual-pdf-theme.yml -r ./rouge_theme.rb MasterSim_manual.adoc
