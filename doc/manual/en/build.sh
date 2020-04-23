05500a466fc8b6430864cc4b46ecddfd86395c30
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-04-23 10:26:50 +0000

    Added german manual.

--- doc/manual/en/build.sh
+++ doc/manual/en/build.sh
@@ -4,3 +4,8 @@ echo '*** Generating html ***'
 asciidoctor MasterSim_manual.adoc
 echo '*** Generating pdf ***'
 asciidoctor-pdf -a pdf-theme=mastersim-manual-pdf-theme.yml -r ./rouge_theme.rb MasterSim_manual.adoc
+
+mv MasterSim_manual.html ../MasterSim_manual_en.html
+mv MasterSim_manual.pdf ../MasterSim_manual_en.pdf
+cp images/* ../images
+
