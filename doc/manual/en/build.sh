68ecba1a7018d48b55c6adb273792daa86164700
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-11-24 15:04:59 +0000

    Manual directory structure updated. Finished review of chapter 4 in manual.

--- doc/manual/en/build.sh
+++ doc/manual/en/build.sh
@@ -5,7 +5,6 @@ asciidoctor MasterSim_manual.adoc
 echo '*** Generating pdf ***'
 asciidoctor-pdf -a pdf-theme=mastersim-manual-pdf-theme.yml -r ./rouge_theme.rb MasterSim_manual.adoc
 
-mv MasterSim_manual.html ../MasterSim_manual_en.html
+mv MasterSim_manual.html ../html/MasterSim_manual_en.html
 mv MasterSim_manual.pdf ../MasterSim_manual_en.pdf
-cp images/* ../images
 
