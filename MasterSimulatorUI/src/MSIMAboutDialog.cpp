7aeeb4abecfaab04daeb8c059d50c70e68a86dd4
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-09-07 03:48:17 +0000

    Link to webpage added to about box. Version number increased to 0.8.4

--- MasterSimulatorUI/src/MSIMAboutDialog.cpp
+++ MasterSimulatorUI/src/MSIMAboutDialog.cpp
@@ -10,6 +10,22 @@ MSIMAboutDialog::MSIMAboutDialog(QWidget *parent) :
 	m_ui->setupUi(this);
 
 	setWindowTitle(QString("MasterSimulator %1").arg(MASTER_SIM::LONG_VERSION));
+
+	QString labelStyle(
+				"font-size:10pt; color: #2121a6; text-decoration:none"
+				);
+
+	QLabel * linkLabel = new QLabel( QString("<a href=\"https://bauklimatik-dresden.de/mastersim\"><span style=\"%1\">https://bauklimatik-dresden.de/mastersim</span></a>").arg(labelStyle));
+	linkLabel->setParent(this);
+	linkLabel->resize(400,25);
+	linkLabel->setAutoFillBackground(false);
+	linkLabel->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+	linkLabel->setOpenExternalLinks(true);
+	linkLabel->move(28,487);
+	linkLabel->setAttribute(Qt::WA_TranslucentBackground);
+
+	layout()->setMargin(0);
+	layout()->setSizeConstraint( QLayout::SetFixedSize );
 }
 
 
