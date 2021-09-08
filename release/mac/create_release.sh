88f8cbbd2ca12c895ed42976e02698ba6dae82eb
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-09-08 21:28:53 +0200

    Cleanup of imported repo

--- release/mac/create_release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH=~/Qt/4.8.7/bin:$PATH
-python ../scripts/createRelease.py --product MasterSimulator --constants-path ../../MasterSim/src  $*
-
--- release/win/deploy.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set INNO_SETUP_PATH="c:\Program Files (x86)\Inno Setup 5\ISCC.exe"
-
-%INNO_SETUP_PATH% deploy.iss
-
