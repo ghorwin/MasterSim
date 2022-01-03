32b433bccabf161c369720e1dac6cbae2d9a3427
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2022-01-03 17:59:53 +0100

    Created 0.9.2 release (first release for public Ubuntu repo)

--- MasterSim/src/MSIM_Constants.cpp
+++ MasterSim/src/MSIM_Constants.cpp
@@ -4,7 +4,7 @@ namespace MASTER_SIM {
 
 const char * const VERSION = "0.9";
 
-const char * const LONG_VERSION = "0.9.1";
+const char * const LONG_VERSION = "0.9.2";
 
 const unsigned int KEYWORD_INDENTATION = 0;
 
