b168a25f0d23576ccdf9fc070251b818287f22f1
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2022-01-04 18:03:33 +0100

    Updated documentation for debian package building.

--- debian/scripts/update_repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "MasterSim-git" ]; then
-  echo "Cloning github repo"
-  git clone https://github.com/ghorwin/MasterSim.git MasterSim-git
-else
-  echo "Reverting local changes and pulling newest revisions from github"
-  (cd MasterSim-git; git reset --hard HEAD; git pull --rebase)
-fi
