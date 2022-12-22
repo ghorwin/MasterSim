1c01dd9965e1a9ce6ea97914426786d50206b02e
Andreas Nicolai <A.Nicolai@uit-gmbh.de>
2022-12-22 18:30:51 +0100

    Preparing release - version number increase, man page updated

--- MasterSim/src/MSIM_Constants.cpp
+++ MasterSim/src/MSIM_Constants.cpp
@@ -3,8 +3,8 @@
 namespace MASTER_SIM {
 
 const char * const VERSION			= "0.9";
-const char * const LONG_VERSION		= "0.9.4";
-const char * const RELEASE_DATE		= "January 23rd, 2022";
+const char * const LONG_VERSION		= "0.9.5";
+const char * const RELEASE_DATE		= "Dezember 23rd, 2022";
 
 const unsigned int KEYWORD_INDENTATION = 0;
 
