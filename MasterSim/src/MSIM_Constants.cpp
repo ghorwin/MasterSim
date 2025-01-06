f140b53cc9c9e92053666054a9a640a04de60163
Andreas Nicolai <andreas.nicolai@gmx.net>
2025-01-06 22:59:45 +0100

    Version increased for next release version

--- MasterSim/src/MSIM_Constants.cpp
+++ MasterSim/src/MSIM_Constants.cpp
@@ -3,7 +3,7 @@
 namespace MASTER_SIM {
 
 const char * const VERSION			= "0.9";
-const char * const LONG_VERSION		= "0.9.7";
+const char * const LONG_VERSION		= "0.9.8";
 const char * const RELEASE_DATE		= "December 31st, 2024";
 
 const unsigned int KEYWORD_INDENTATION = 0;
